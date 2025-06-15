028fd3d004ac4919fcc7d6daa22b5d229110bf88
Yang <yuyangchee98@gmail.com>
2025-06-15 21:15:04 +0900

    perf

--- vite.config.ts
+++ vite.config.ts
@@ -8,11 +8,28 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
+    // Enable minification and tree-shaking
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true
+      }
+    },
+    // Chunk size warnings
+    chunkSizeWarningLimit: 500,
     rollupOptions: {
       output: {
         entryFileNames: 'assets/[name].[hash].js',
         chunkFileNames: 'assets/[name].[hash].js',
-        assetFileNames: 'assets/[name].[hash].[ext]'
+        assetFileNames: 'assets/[name].[hash].[ext]',
+        // Manual chunks for better caching
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom', 'react-redux'],
+          'dnd': ['react-dnd', 'react-dnd-html5-backend'],
+          'charts': ['chart.js', 'react-chartjs-2'],
+          'redux': ['@reduxjs/toolkit']
+        }
       }
     }
   },
