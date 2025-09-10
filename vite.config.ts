7ab0ec25e24069cbf545a7a38f9f2e5133a9d386
Yang <yuyangchee98@gmail.com>
2025-09-10 13:55:15 +0900

    automated sitemap

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
+import sitemap from 'vite-plugin-sitemap'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    sitemap({
+      hostname: 'https://f1pointscalculator.yaaaang.com',
+      dynamicRoutes: ['/'],
+      changefreq: 'weekly',
+      priority: 1.0,
+      lastmod: new Date().toISOString().split('T')[0]
+    })
+  ],
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
